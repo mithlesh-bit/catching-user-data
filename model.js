ce00e1567236290d21cc809537aed562ac1f1298
mithlesh-bit <rawtemithlesh52@gmail.com>
2024-03-05 00:41:56 +0530

    device storecddscddddddddddddd

--- model.js
+++ model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const interactionSchema = new mongoose.Schema({
+    userSessionID: String,
     deviceType: String,
     adminID: String,
     browserName: String,
