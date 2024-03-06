fdfe1f36248ca8f62e12703adc4733c39239a431
mithlesh-bit <rawtemithlesh52@gmail.com>
2024-03-06 16:12:04 +0530

    jsfn

--- model.js
+++ model.js
@@ -15,7 +15,7 @@ const interactionSchema = new mongoose.Schema({
     value: String, // Optional, only for 'input' events
     userToken: String,
     deviceType: String // Add this line to include device type information
-}, { timestamps: true });
+});
 
 const Interaction = mongoose.model('Interaction', interactionSchema);
 
